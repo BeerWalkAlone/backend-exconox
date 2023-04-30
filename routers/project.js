de8f118fb989077bab6ecee2ae77929cd550d4b4
BeerWalkAlone <sinnakhon@hotmail.com>
2023-04-30 14:15:11 +0700

    iron

--- routers/project.js
+++ routers/project.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://font-end-project-exconox.vercel.app',
     credentials: true
 }))
 
